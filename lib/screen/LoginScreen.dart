a46944bae568469c4428e60e532fc486fc014c28
thanhdinhvan <dinhvanthanh2510@gmail.com>
2021-07-13 06:30:33 +0700

    UI login

--- lib/screen/LoginScreen.dart
+++ lib/screen/LoginScreen.dart
@@ -11,6 +11,8 @@ class _LoginScreenState extends State<LoginScreen>
     with SingleTickerProviderStateMixin {
   late AnimationController _controller;
 
+  bool _showPass = false;
+
   @override
   void initState() {
     super.initState();
@@ -25,6 +27,108 @@ class _LoginScreenState extends State<LoginScreen>
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.fromLTRB(30, 70, 30, 0),
+        constraints: BoxConstraints.expand(),
+        color: Colors.white,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 50, 0, 0),
+                child: Image.asset('crossover.png'),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 15, 0, 0),
+                child: Text(
+                  'Welcome Back!',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(5),
+                child: Text(
+                  'Login to continue using iCab',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 50, 0, 0),
+                child: Container(
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(3, 0, 0, 0),
+                    child: TextField(
+                        decoration: InputDecoration(
+                            labelText: "User Name",
+                            labelStyle: TextStyle(
+                                color: Color(0xff888888), fontSize: 15))),
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(6),
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(color: Color(0xff888888), spreadRadius: 0.5),
+                    ],
+                  ),
+                  height: 50,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+                child: Container(
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(3, 3, 3, 0),
+                    child: Stack(
+                      alignment: AlignmentDirectional.centerEnd,
+                      children: [
+                        TextField(
+                            obscureText: !_showPass,
+                            decoration: InputDecoration(
+                                labelText: "PASWWORD",
+                                labelStyle: TextStyle(
+                                    color: Color(0xff888888), fontSize: 15)),
+                            style:
+                                TextStyle(fontSize: 18, color: Colors.black)),
+                        GestureDetector(
+                          onTap: onToggleShowPass,
+                          child: Text(
+                            _showPass ? "HiDe" : "Show",
+                            style: TextStyle(
+                                color: Colors.blue,
+                                fontSize: 13,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(6),
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(color: Color(0xff888888), spreadRadius: 0.5),
+                    ],
+                  ),
+                  height: 50,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void onToggleShowPass() {
+    setState(() {
+      _showPass = !_showPass;
+    });
   }
 }
